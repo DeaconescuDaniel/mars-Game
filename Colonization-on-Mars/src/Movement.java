287b658876169002081a977cc7a2279503f13528
DeaconescuDaniel <134158502+DeaconescuDaniel@users.noreply.github.com>
2023-11-25 14:30:51 +0200

    Delete Colonization-on-Mars directory

--- Colonization-on-Mars/src/Movement.java
+++
@@ -1,9 +0,0 @@
-public interface Movement {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
-    public boolean isEnginner();
-    public boolean isColonist();
-    public boolean isMedic();
-}
